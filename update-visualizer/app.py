4f5a5a9a13c025751f4039d72e4efd5b7350b0a9
Michael McCune <msm@redhat.com>
2018-11-08 14:54:54 -0500

    modify visualizer to read from kafka stream
    This changes the basic functionality so that the visualizer can now read
    from a kafka stream to gain its information. This is being done as part
    of an effort to streamline the lab experience and remove the
    update-analyzer.

--- update-visualizer/app.py
+++ update-visualizer/app.py
@@ -1,11 +1,15 @@
+import argparse
 import copy
 import logging
+import logging.config as lconfig
 import math
+import os
 import threading
 
 import flask
 from flask import views
 from flask import json
+import kafka
 
 
 class LastData():
@@ -49,6 +53,7 @@ class LastData():
         return retval
 
 
+exit_event = threading.Event()
 _last_data = LastData()
 
 
@@ -62,25 +67,77 @@ class RootView(views.MethodView):
     def get(self):
         return json.jsonify(last_data())
 
-    def post(self):
-        data = flask.request.data
-        last_data(json.loads(data))
-        return ('', 202)
+
+def consumer(args):
+    logging.info('starting kafka consumer')
+    consumer = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
+    for msg in consumer:
+        if exit_event.is_set():
+            break
+        try:
+            last_data(json.loads(str(msg.value, 'utf-8')))
+        except Exception as e:
+            logging.error(e.message)
+    logging.info('exiting kafka consumer')
 
 
-def main():
+def get_arg(env, default):
+    return os.getenv(env) if os.getenv(env, '') is not '' else default
+
+
+def parse_args(parser):
+    args = parser.parse_args()
+    args.brokers = get_arg('KAFKA_BROKERS', args.brokers)
+    args.topic = get_arg('KAFKA_TOPIC', args.topic)
+    return args
+
+
+def main(args):
+    exit_event.clear()
+    # setup consumer thread
+    cons = threading.Thread(group=None, target=consumer, args=(args,))
+    cons.start()
+
     # create the flask app object
     app = flask.Flask(__name__)
     # change this value for production environments
     app.config['SECRET_KEY'] = 'secret!'
-
     app.add_url_rule('/', view_func=RootView.as_view('index'))
-
     app.run(host='0.0.0.0', port=8080)
+
+    exit_event.set()
+    cons.join()
+    logging.info('exiting flask-kafka-listener')
 
 
 if __name__ == '__main__':
+    lconfig.dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+            }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+            }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+            }
+        })
     logging.basicConfig(level=logging.INFO)
     logging.info('starting update-visualizer')
-    main()
+    parser = argparse.ArgumentParser(
+            description='listen for some stuff on kafka')
+    parser.add_argument(
+            '--brokers',
+            help='The bootstrap servers, env variable KAFKA_BROKERS',
+            default='kafka.kafka.svc:9092')
+    parser.add_argument(
+            '--topic',
+            help='Topic to publish to, env variable KAFKA_TOPIC',
+            default='social-firehose')
+    args = parse_args(parser)
+    main(args)
     logging.info('exiting')
